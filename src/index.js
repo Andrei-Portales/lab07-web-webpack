39f7cc536b5d117374c668d794980decde0e4cba
Andrei-Portales <30614183+Andrei-Portales@users.noreply.github.com>
2021-02-22 22:48:06 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap';
 import './main.scss';
 import 'animate.css';
 import createApp from './app/app';
-
+// npm install --save jquery popper.js
 createApp();
